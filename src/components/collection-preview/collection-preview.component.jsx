2942b909af2fbaa0c7ff01453b7c09894f6ef648
Andrew Lilley <andrew.c.lilley@gmail.com>
2020-06-04 09:52:50 +0100

    Add add to cart functionality

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -10,8 +10,8 @@ const CollectionPreview = ({ title, items }) => (
         items
         // Only display 4 items in the preview.
         .filter((item, idx) => idx < 4)
-        .map(({ id, ...otherItemProps }) => (
-          <CollectionItem key={id} {...otherItemProps} />
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
         ))
       }
     </div>
