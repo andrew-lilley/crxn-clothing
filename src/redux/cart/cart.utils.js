7ff9e2f275fd612505470355c8041573672c3d3f
Andrew Lilley <andrew.c.lilley@gmail.com>
2020-06-05 10:38:46 +0100

    Add add item, remove item and clear item functionality to the checkout list component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -6,10 +6,7 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   if (existingCartItem) {
     return cartItems.map(cartItem => 
       cartItem.id === cartItemToAdd.id 
-      ? { 
-          ...cartItem, 
-          quantity: cartItem.quantity + 1 
-        }
+      ? { ...cartItem, quantity: cartItem.quantity + 1 }
       : cartItem
     )
   }
@@ -18,4 +15,23 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     ...cartItems, 
     { ...cartItemToAdd, quantity: 1 }
   ]
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  // Check if the qty is 1. If so, we need to remove it from the cart
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(
+      cartItem => cartItem.id !== cartItemToRemove.id
+    )
+  }
+
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  )
 };
