a7b10f3a333664c18fe52d8f0166cd19ef80fb47
Andrew Lilley <andrew.c.lilley@gmail.com>
2020-06-05 16:30:13 +0100

    Add redux persist, move static shop data and created respective reducers and selectors to return the data. Created collection pages.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import thunk from "redux-thunk";
 import rootReducer from './root-reducer';
@@ -10,9 +11,11 @@ const middlewares = [logger, thunk];
 
 composeEnhancers(applyMiddleware(thunk))
 
-const store = createStore(
+export const store = createStore(
   rootReducer, 
   composeEnhancers(applyMiddleware(...middlewares))
 );
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
