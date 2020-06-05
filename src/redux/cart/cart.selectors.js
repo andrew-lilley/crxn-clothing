4143b6afc2434abd38d6ed572a2110f8d9e69168
Andrew Lilley <andrew.c.lilley@gmail.com>
2020-06-05 09:57:33 +0100

    Add base checkout page, checkout items component and a cart total selector

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -20,3 +20,12 @@ export const selectCartItemsCount = createSelector(
       0
     )
 );
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (cartTotal, cartItem) => cartTotal + (cartItem.price * cartItem.quantity),
+      0
+    )
+);
